6243288dcfc3001bd361dc0569c86aadbac1dc44
ShadyCoding <shahidmahmood.yar@gmail.com>
2020-04-25 01:24:46 +0500

    Initial commit

---
+++ app/src/main/java/com/shady/togglebutton/MainActivity.java
@@ -0,0 +1,34 @@
+package com.shady.togglebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    private ToggleButton toggleButton;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = findViewById(R.id.toggleButton);
+        tvResult = findViewById(R.id.peek);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    tvResult.setVisibility(View.VISIBLE);
+                }else {
+                    tvResult.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+}
